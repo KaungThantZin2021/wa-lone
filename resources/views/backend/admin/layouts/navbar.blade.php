e4696fa288301cc1cbd7357644a06737f26b4444
KaungThantZin2021 <kaungthantzin409@gmail.com>
2022-08-08 09:14:59 +0630

    laravel mix on going

--- resources/views/backend/admin/layouts/navbar.blade.php
+++ resources/views/backend/admin/layouts/navbar.blade.php
@@ -166,7 +166,7 @@
             <li class="nav-item dropdown">
                 <a class="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
                     <div class="navbar-profile">
-                        <img class="img-xs rounded-circle" src="{{ asset('assets/images/faces/face15.jpg') }}"
+                        <img class="img-xs rounded-circle" src="{{ asset('backend/assets/images/faces/face15.jpg') }}"
                             alt="">
                         <p class="mb-0 d-none d-sm-block navbar-profile-name">{{ auth()->guard('admin_user')->user()->name }}
                         </p>
